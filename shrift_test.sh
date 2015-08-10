b654f7f5944bee1faebee144efdb8b835d065153
nextrevision <notarobot@nextrevision.net>
2015-08-10 10:53:01 -0400

    Colorizing commands based on result and surrounding exit code in parentheses

--- shrift_test.sh
+++ shrift_test.sh
@@ -7,7 +7,7 @@ source ./shrift
 assert "_usage | head -n1 | cut -d ' ' -f1" "usage:"
 
 # test _error
-assert "_error test" "${RED}ERROR${END}: test"
+assert "_error test" "${RED}ERROR: test${END}"
 
 # test _mktemp
 t=$(_mktemp)
@@ -15,9 +15,9 @@ assert_raises "test -f ${t}" 0
 rm $t
 
 # test _print_cmd_summary
-assert "_print_cmd_summary 0 'good'" "  ${GREEN}0${END}: good"
-assert "_print_cmd_summary 1 'bad'" "  ${RED}1${END}: bad"
-assert "_print_cmd_summary 255 'also bad'" "  ${RED}255${END}: also bad"
+assert "_print_cmd_summary 0 'good'" "  (${GREEN}0${END}) ${GREEN}good${END}"
+assert "_print_cmd_summary 1 'bad'" "  (${RED}1${END}) ${RED}bad${END}"
+assert "_print_cmd_summary 255 'also bad'" "  (${RED}255${END}) ${RED}also bad${END}"
 
 # test _print_cmd_output
 t=$(_mktemp)
@@ -34,11 +34,11 @@ assert "_print_dot 255" "${RED}F${END}"
 
 # test _main output
 assert "_main test/fixtures/pass_spec.sh" "\n${GREEN}.${END}${GREEN}.${END}\n2 tests, 0 failed"
-assert "_main test/fixtures/fail_spec.sh" "\n  ${RED}1${END}: test -f notpresent\n\n${RED}F${END}\n1 tests, 1 failed"
-assert "_main -v test/fixtures/pass_spec.sh" "\n  ${GREEN}0${END}: test -f ./shrift\n  ${GREEN}0${END}: test -f ./shrift_test.sh\n\n${GREEN}.${END}${GREEN}.${END}\n2 tests, 0 failed"
-assert "_main test/fixtures" "\n  ${RED}1${END}: test -f notpresent\n\n${RED}F${END}${GREEN}.${END}${GREEN}.${END}\n3 tests, 1 failed"
-assert "_main test/**/*_spec.sh" "\n  ${RED}1${END}: test -f notpresent\n\n${RED}F${END}${GREEN}.${END}${GREEN}.${END}\n3 tests, 1 failed"
-assert "_main test/fixtures/*_spec.sh" "\n  ${RED}1${END}: test -f notpresent\n\n${RED}F${END}${GREEN}.${END}${GREEN}.${END}\n3 tests, 1 failed"
+assert "_main test/fixtures/fail_spec.sh" "\n  (${RED}1${END}) ${RED}test -f notpresent${END}\n\n${RED}F${END}\n1 tests, 1 failed"
+assert "_main -v test/fixtures/pass_spec.sh" "\n  (${GREEN}0${END}) ${GREEN}test -f ./shrift${END}\n  (${GREEN}0${END}) ${GREEN}test -f ./shrift_test.sh${END}\n\n${GREEN}.${END}${GREEN}.${END}\n2 tests, 0 failed"
+assert "_main test/fixtures" "\n  (${RED}1${END}) ${RED}test -f notpresent${END}\n\n${RED}F${END}${GREEN}.${END}${GREEN}.${END}\n3 tests, 1 failed"
+assert "_main test/**/*_spec.sh" "\n  (${RED}1${END}) ${RED}test -f notpresent${END}\n\n${RED}F${END}${GREEN}.${END}${GREEN}.${END}\n3 tests, 1 failed"
+assert "_main test/fixtures/*_spec.sh" "\n  (${RED}1${END}) ${RED}test -f notpresent${END}\n\n${RED}F${END}${GREEN}.${END}${GREEN}.${END}\n3 tests, 1 failed"
 
 # test runtime usage
 assert_raises "./shrift -h" 0
@@ -46,8 +46,8 @@ assert_raises "./shrift -h | grep -q 'usage: '" 0
 
 # test runtime output
 assert "./shrift test/fixtures/pass_spec.sh" "\n${GREEN}.${END}${GREEN}.${END}\n2 tests, 0 failed\n"
-assert "./shrift test/fixtures/fail_spec.sh" "\n  ${RED}1${END}: test -f notpresent\n\n${RED}F${END}\n1 tests, 1 failed\n"
-assert "./shrift -v test/fixtures/pass_spec.sh" "\n  ${GREEN}0${END}: test -f ./shrift\n  ${GREEN}0${END}: test -f ./shrift_test.sh\n\n${GREEN}.${END}${GREEN}.${END}\n2 tests, 0 failed\n"
+assert "./shrift test/fixtures/fail_spec.sh" "\n  (${RED}1${END}) ${RED}test -f notpresent${END}\n\n${RED}F${END}\n1 tests, 1 failed\n"
+assert "./shrift -v test/fixtures/pass_spec.sh" "\n  (${GREEN}0${END}) ${GREEN}test -f ./shrift${END}\n  (${GREEN}0${END}) ${GREEN}test -f ./shrift_test.sh${END}\n\n${GREEN}.${END}${GREEN}.${END}\n2 tests, 0 failed\n"
 
 # test runtime return code
 assert_raises "./shrift test/fixtures/pass_spec.sh" 0
